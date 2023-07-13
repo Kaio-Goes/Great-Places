08ce55968cc1499038e02d45a8c7ab2bf1eeb6e6
Kaio Goes <kaio.goes@spezi.com.br>
2023-07-13 17:21:08 -0300

    Feature imagePicker in form

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,6 +11,19 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  void _takePicture() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? imageFile = await picker.pickImage(source: ImageSource.camera);
+
+    if (imageFile != null) {
+      setState(() {
+        _storedImage = File(imageFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -18,14 +34,20 @@ class _ImageInputState extends State<ImageInput> {
           decoration:
               BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
           alignment: Alignment.center,
-          child: const Text('Nenhuma imagem!'),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : const Text('Nenhuma imagem!'),
         ),
         const SizedBox(
           width: 10,
         ),
         Expanded(
           child: TextButton.icon(
-              onPressed: () {},
+              onPressed: _takePicture,
               icon: const Icon(Icons.camera),
               label: const Text('Tirar Foto')),
         ),
