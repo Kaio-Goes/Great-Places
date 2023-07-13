2afdf2548876360628d7c69d1d8a98d8765f3b17
Kaio Goes <kaio.goes@spezi.com.br>
2023-07-13 16:55:09 -0300

    Feature form camera

--- lib/screens/places_form_screen.dart
+++ lib/screens/places_form_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/widgets/image_input.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   const PlaceFormScreen({super.key});
@@ -8,6 +9,12 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm (){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +22,42 @@ class _PlaceFormScreenState extends State<PlaceFormScreen> {
         title: const Text('Novo Lugar'),
         centerTitle: true,
       ),
-      body: const  Center(
-        child: Text('Form'),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children:  [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const ImageInput()
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: _submitForm,
+            icon: const Icon(Icons.add),
+            label: const Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+            ),
+          )
+        ],
       ),
     );
   }
